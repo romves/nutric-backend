68ccaaf41f4f6589d48680adf41a1bb7a3b72755
rmvs <nofalrmz@gmail.com>
2024-11-12 01:23:53 +0700

    feat: remove User module and service, add HealthInfo module, and implement JwtAuthGuard for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,17 @@ import { PrismaModule } from './common/db/prisma/prisma.module';
 import { AuthModule } from './module/auth/auth.module';
 import { GeminiModule } from './module/gemini/gemini.module';
 import { FoodModule } from './module/food/food.module';
-import { UserModule } from './module/user/user.module';
 import { MealModule } from './module/meal/meal.module';
+import { HealthInfoModule } from './module/health-info/health-info.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
-    UserModule,
     GeminiModule,
     FoodModule,
     MealModule,
+    HealthInfoModule,
   ],
 })
 export class AppModule {}
