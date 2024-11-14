9882a7d869d7773c57abf4548bdced872e249ea4
rmvs <nofalrmz@gmail.com>
2024-11-14 23:01:09 +0700

    feat(food): implement food nutrition and name prediction

--- src/module/food/food.module.ts
+++ src/module/food/food.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { NutrientService } from './services/nutrient.service';
+import { GeminiModule } from '../gemini/gemini.module';
+import { HealthInfoModule } from '../health-info/health-info.module';
+import { FoodController } from './controllers/food.controller';
+import { FoodRepository } from './repositories/food.repository';
 import { FoodService } from './services/food.service';
+import { NutrientService } from './services/nutrient.service';
 
 @Module({
-  providers: [NutrientService, FoodService]
+  providers: [NutrientService, FoodService, FoodRepository],
+  controllers: [FoodController],
+  imports: [HealthInfoModule, GeminiModule],
 })
 export class FoodModule {}
