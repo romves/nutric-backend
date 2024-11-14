fe94ced6357f044dff8fe93b9c558440cc6bf81e
rmvs <nofalrmz@gmail.com>
2024-11-14 22:58:06 +0700

    feat: enhance error handling in HttpExceptionFilter and update global validation pipe configuration

--- src/config/app/app.config.ts
+++ src/config/app/app.config.ts
@@ -3,7 +3,14 @@ import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
 export const setupGlobalConfig = (app: INestApplication) => {
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    // whitelist: true,
+    // forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.setGlobalPrefix('api/v1');
