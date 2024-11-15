5d5593bb839251725c3d81e53ceae657ff9909ba
rmvs <nofalrmz@gmail.com>
2024-11-16 03:14:42 +0700

    feat(config): add global configuration module for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { GeminiModule } from './module/gemini/gemini.module';
 import { FoodModule } from './module/food/food.module';
 import { MealModule } from './module/meal/meal.module';
 import { HealthInfoModule } from './module/health-info/health-info.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     PrismaModule,
     AuthModule,
     GeminiModule,
